b3426c4fe1b5253018629756664db3788c7d23a0
YiJiangFengYun <yijiangfengyun@outlook.com>
2020-12-11 11:44:12 +0800

    Update

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-console.info(`This is a project of learning pm2.`);
+function output() {
+    console.info(`This is a project of learning pm2.`);
 
-console.info(`COMMON_VARIABLE: ${process.env.COMMON_VARIABLE}`);
-console.info(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.info(`DEPLOY_PRODUCTION: ${process.env.DEPLOY_PRODUCTION}`);
+    console.info(`COMMON_VARIABLE: ${process.env.COMMON_VARIABLE}`);
+    console.info(`NODE_ENV: ${process.env.NODE_ENV}`);
+    console.info(`DEPLOY_PRODUCTION: ${process.env.DEPLOY_PRODUCTION}`);
+}
+
+setInterval(() => {
+    output();
+}, 5000);
