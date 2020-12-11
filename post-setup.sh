0b837caae0dc118e5901ff7b91aaaa5a9fb9d752
YiJiangFengYun <yijiangfengyun@outlook.com>
2020-12-11 10:58:39 +0800

    Update

---
+++ post-setup.sh
@@ -0,0 +1,3 @@
+echo "Commands or a script path to be run on the host after cloning the repo"
+echo "User home is: "
+echo $HOME
---
+++ pre-setup.sh
@@ -0,0 +1,6 @@
+echo "Commands or a script path to be run on the host before the setup process starts"
+echo "User home is: "
+echo $HOME
+echo "CWD: "
+pwd
+rm -rf ~/deploy
